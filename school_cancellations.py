f6c4e6a7ec8f6e66c73c3975e5dbd748663c4bdd
seanthony <sean@basecampcodingacademy.org>
2018-11-14 10:57:32 -0600

    gets row data out. need to strip all tags next

--- school_cancellations.py
+++ school_cancellations.py
@@ -2,13 +2,20 @@ import requests
 
 
 def get_page_text():
-    r = requests.get('https://www.wtva.com/weather/closings/')
+    # r = requests.get('https://www.wtva.com/weather/closings/')
+    r = requests.get('https://ftp2.wtva.com/All_Active.html')
     return r.text
+
+
+def scrape_page(text):
+    rows = list(map(lambda r: r.split('</tr>')[0], text.split('<tr>')))
+    print('school closings:\n\t', end="")
+    print('\n\t'.join(rows))
 
 
 def main():
     text = get_page_text()
-    # scrape_page(text)
+    scrape_page(text)
 
 
 if __name__ == "__main__":
